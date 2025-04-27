6b785a32ebe675ae4b0fdc093c3324cbc5970edc
jainavinash511 <jain.avinash511@gmail.com>
2025-04-27 17:00:30 +0530

    Basic Calculator Logic

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,34 @@
-alert("Working");
+let currentResult = 0;
+
+function add() {
+  const initialVal = currentResult;
+  const currentVal = parseInt(userInput.value);
+  currentResult += currentVal;
+  outputResult(currentResult, `${initialVal} + ${currentVal}`);
+}
+
+function substract() {
+  const initialVal = currentResult;
+  const currentVal = parseInt(userInput.value);
+  currentResult -= currentVal;
+  outputResult(currentResult, `${initialVal} - ${currentVal}`);
+}
+
+function multiply() {
+  const initialVal = currentResult;
+  const currentVal = parseInt(userInput.value);
+  currentResult *= currentVal;
+  outputResult(currentResult, `${initialVal} * ${currentVal}`);
+}
+
+function divide() {
+  const initialVal = currentResult;
+  const currentVal = parseInt(userInput.value);
+  currentResult /= currentVal;
+  outputResult(currentResult, `${initialVal} / ${currentVal}`);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
