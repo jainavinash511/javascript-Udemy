85bba59d5ebfeae48a87adf220e25fa893fc0bdc
jainavinash511 <jain.avinash511@gmail.com>
2025-04-30 20:20:45 +0530

    Heal Player Functionality

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK = 20;
+const HEAL_VALUE = 30;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -8,15 +9,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMode(mode) {
-  let damageValue;
-  if (mode === "ATTACK") {
-    damageValue = ATTACK_VALUE;
-  } else {
-    damageValue = STRONG_ATTACK;
-  }
-  const damage = dealMonsterDamage(damageValue);
-  currentMonsterHealth -= damage;
+function endGame() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
 
@@ -29,11 +22,37 @@ function attackMode(mode) {
   }
 }
 
+function attackMode(mode) {
+  let damageValue;
+  if (mode === "ATTACK") {
+    damageValue = ATTACK_VALUE;
+  } else {
+    damageValue = STRONG_ATTACK;
+  }
+  const damage = dealMonsterDamage(damageValue);
+  currentMonsterHealth -= damage;
+  endGame();
+}
+
 function attackHandler() {
   attackMode("ATTACK");
 }
 function strongHandler() {
   attackMode("STRONG_ATTACK");
 }
+function healPlayerHandler() {
+  let healvalue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert("Cant go higher than MAX");
+    healvalue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healvalue = HEAL_VALUE;
+  }
+  currentPlayerHealth += healvalue;
+  increasePlayerHealth(healvalue);
+  endGame();
+}
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongHandler);
+healBtn.addEventListener("click", healPlayerHandler);
