4e9dd56dfe5d84e4ef1db5f3c1496e1f82110286
jainavinash511 <jain.avinash511@gmail.com>
2025-05-13 18:50:13 +0530

    added movie Object

--- Movies/assets/scripts/objects.js
+++ Movies/assets/scripts/objects.js
@@ -4,7 +4,22 @@ const addHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
   const extraValue = document.getElementById("extra-value").value;
-  console.log(title, extraName, extraValue);
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+  const movie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+  console.log(movie);
 };
 
 addbtn.addEventListener("click", addHandler);
