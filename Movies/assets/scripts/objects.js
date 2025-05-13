946b7eb71197da349048940dd0126c90da2b4444
jainavinash511 <jain.avinash511@gmail.com>
2025-05-13 20:00:53 +0530

    Display list functinality

--- Movies/assets/scripts/objects.js
+++ Movies/assets/scripts/objects.js
@@ -1,4 +1,22 @@
 const addbtn = document.getElementById("add-movie-btn");
+const movies = [];
+
+const renderMovieList = () => {
+  const movieList = document.getElementById("movie-list");
+  console.log(movieList);
+  movieList.innerHTML = "";
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+  } else {
+    movieList.classList.add("visible");
+  }
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+  console.log(movieList);
+};
 
 const addHandler = () => {
   const title = document.getElementById("title").value;
@@ -19,7 +37,9 @@ const addHandler = () => {
     },
     id: Math.random(),
   };
-  console.log(movie);
+  movies.push(movie);
+  renderMovieList();
+  console.log(movies);
 };
 
 addbtn.addEventListener("click", addHandler);
