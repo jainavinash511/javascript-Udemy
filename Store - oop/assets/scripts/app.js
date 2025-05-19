aa7dc24a6cb5a95f20ae34de4919a1bc77f1d01e
jainavinash511 <jain.avinash511@gmail.com>
2025-05-19 18:05:25 +0530

    added logic without image

--- Store - oop/assets/scripts/app.js
+++ Store - oop/assets/scripts/app.js
@@ -6,3 +6,33 @@ class product {
     this.desc = desc;
   }
 }
+
+class ProductList {
+  products = [
+    new product("Pillow", "", 19.99, "A Nice Pillow"),
+    new product("Carpet", "", 89.99, "A Nice Carpet"),
+  ];
+  render() {
+    const displayList = document.getElementById("app");
+    const productsEle = document.createElement("ul");
+    productsEle.className = "product-list";
+    for (let prod of this.products) {
+      const prodEle = document.createElement("li");
+      prodEle.className = "product-item";
+      prodEle.innerHTML = `
+        <div>
+            <div class ="product-item__content">
+            <h2>${prod.title}</h2>
+            <p>${prod.price}</p>
+            <p>${prod.desc}</p>
+            <button> Add To Cart </button>
+            <div>
+        </div>
+        `;
+      productsEle.append(prodEle);
+    }
+    displayList.append(productsEle);
+  }
+}
+const Prod = new ProductList();
+Prod.render();
